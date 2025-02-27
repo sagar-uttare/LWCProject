429a9a430d7f7166519abb858dbb10368d50898f
Sagar Uttare <sagarruttare@gmail.com>
2025-02-27 18:44:16 +0530

    wire update

--- force-app/main/default/lwc/wireAdapter/wireAdapter.js
+++ force-app/main/default/lwc/wireAdapter/wireAdapter.js
@@ -1,8 +1,10 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,wire } from 'lwc';
 import {getRecord,getFieldValue} from 'lightning/uiRecordApi';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import PHONE_FIELD from '@salesforce/schema/Account.Phone';
 export default class WireAdapter extends LightningElement {
     @api recordId;
     
+    @wire(getRecord,{recordId:'$recordId',fields:[NAME_FIELD,PHONE_FIELD]})
+    record;
 }
